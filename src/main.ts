61c3c67ad4736d364db4fb7c29a51bb400d8c88e
kaltre10 <kaltre10@gmail.com>
2024-01-10 01:44:05 -0500

    Ajustes generales de codigo, se agrega readme base, se agrega vite.config.js

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,4 @@
-import './style.css';
 import getName from './getName.ts'
-
-// Obtener los elementos del DOM
-const videoElement: HTMLVideoElement | null = document.querySelector('#video');
-const startButton: HTMLButtonElement | null = document.querySelector('#rec');
-const stopButton:  HTMLButtonElement | null = document.querySelector('#stop');
 
 // Variables para guardar el stream y el objeto MediaRecorder
 let mediaStream: MediaStream;
@@ -13,40 +7,38 @@ let chunks: Blob[] = [];
 
 // Función para comenzar la grabación
 function startRecording(): void {
-  navigator.mediaDevices.getDisplayMedia({ video: true, audio: true })
-    .then((stream: MediaStream) => {
-      mediaStream = stream;
-      if (videoElement) videoElement.srcObject = stream;
+navigator.mediaDevices.getDisplayMedia({ video: true, audio: true })
+  .then((stream: MediaStream) => {
+    mediaStream = stream;
+    // Crear el objeto MediaRecorder para grabar el video
+    mediaRecorder = new MediaRecorder(stream);
+    chunks = [];
 
-      // Crear el objeto MediaRecorder para grabar el video
-      mediaRecorder = new MediaRecorder(stream);
-      chunks = [];
+    // Evento que se dispara cada vez que se graba un fragmento de video
+    mediaRecorder.ondataavailable = (e: BlobEvent) => chunks.push(e.data);
 
-      // Evento que se dispara cada vez que se graba un fragmento de video
-      mediaRecorder.ondataavailable = (e: BlobEvent) => chunks.push(e.data);
+    // Evento que se dispara cuando la grabación se detiene
+    mediaRecorder.onstop = () => {
+      // Crear un objeto Blob a partir de los fragmentos grabados
+      const blob: Blob = new Blob(chunks, { type: 'video/webm' });
 
-      // Evento que se dispara cuando la grabación se detiene
-      mediaRecorder.onstop = () => {
-        // Crear un objeto Blob a partir de los fragmentos grabados
-        const blob: Blob = new Blob(chunks, { type: 'video/webm' });
+      // Crear un enlace de descarga para el video grabado
+      const url: string = URL.createObjectURL(blob);
+      const a: HTMLAnchorElement = document.createElement('a');
+      a.href = url;
+      a.download = getName()
+      a.click();
 
-        // Crear un enlace de descarga para el video grabado
-        const url: string = URL.createObjectURL(blob);
-        const a: HTMLAnchorElement = document.createElement('a');
-        a.href = url;
-        a.download = getName()
-        a.click();
+      // Liberar el objeto URL
+      URL.revokeObjectURL(url);
+    };
 
-        // Liberar el objeto URL
-        URL.revokeObjectURL(url);
-      };
-
-      // Comenzar la grabación
-      mediaRecorder.start();
-    })
-    .catch((err: Error) => {
-      console.error('Error al capturar la pantalla:', err);
-    });
+    // Comenzar la grabación
+    mediaRecorder.start();
+  })
+  .catch((err: Error) => {
+    console.error('Error al capturar la pantalla:', err);
+  });
 }
 
 // Función para detener la grabación
@@ -57,10 +49,10 @@ function stopRecording(): void {
 
     // Detener el stream de captura
     mediaStream.getTracks().forEach((track: MediaStreamTrack) => track.stop());
-    if (videoElement) videoElement.srcObject = null;
   }
 }
 
-// Asociar los eventos de clic a los botones
-if (startButton) startButton.addEventListener('click', startRecording);
-if (stopButton)  stopButton.addEventListener('click', stopRecording);
+export {
+  startRecording,
+  stopRecording,
+}
